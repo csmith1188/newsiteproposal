6b988a009d3408bfe04e22c1cccf862a1e732873
Parker Fink <parke2023059@live.ytech.edu>
2022-08-29 11:14:54 -0400

    3

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ app.get('/', function(req,res){
   res.render('index.ejs')
 })
 
+app.get('/mediacenter', function(req,res){
+  res.render('mediacenter/mediacenter.ejs')
+})
 
+app.get('/attendance', function(req,res){
+  res.render('attendance/attendance.ejs')
+})
 
 app.listen(port, ip, function(){
   console.log("Site Is Functional");
