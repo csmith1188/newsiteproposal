875c36e0e0f6340ac56849fcdddd9587ec97cae2
Parker Fink <parke2023059@live.ytech.edu>
2022-08-30 14:56:36 -0400

    4

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require('express');
 app = express()
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
 
 const port = 8080
 const ip = '127.0.0.1'
@@ -13,9 +15,31 @@ app.get('/mediacenter', function(req,res){
   res.render('mediacenter/mediacenter.ejs')
 })
 
+app.get('/parents', function(req,res){
+  res.render('parents/parents.ejs')
+})
+
+app.get('/senior_info', function(req,res){
+  res.render('seniorinfo/seniorinfo.ejs')
+})
+
+app.get('/staff', function(req,res){
+  res.render('staff/staff.ejs')
+})
+
 app.get('/attendance', function(req,res){
   res.render('attendance/attendance.ejs')
 })
+
+
+app.get('/', function(req,res){
+  res.render('/main.ejs')
+})
+
+
+
+
+
 
 app.listen(port, ip, function(){
   console.log("Site Is Functional");
