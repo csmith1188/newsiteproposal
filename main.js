9e88d6387bdabe95b6b140d2e123861656d147b3
Parker Fink <parke2023059@live.ytech.edu>
2022-08-31 10:42:53 -0400

    5

--- main.js
+++ main.js
@@ -10,36 +10,34 @@ const ip = '127.0.0.1'
 app.get('/', function(req,res){
   res.render('index.ejs')
 })
-
-app.get('/mediacenter', function(req,res){
-  res.render('mediacenter/mediacenter.ejs')
-})
-
 app.get('/parents', function(req,res){
-  res.render('parents/parents.ejs')
+  res.render('parents.ejs')
+})
+app.get('/senior_info', function(req,res){
+  res.render('seniorinfo.ejs')
+})
+app.get('/attendance', function(req,res){
+  res.render('attendance.ejs')
+})
+app.get('/media_center', function(req,res){
+  res.render('mediacenter.ejs')
 })
 
-app.get('/senior_info', function(req,res){
-  res.render('seniorinfo/seniorinfo.ejs')
+app.get('/athlete', function(req,res){
+  res.render('athlete.ejs')
+})
+
+app.get('/jobs', function(req,res){
+  res.render('jobs.ejs')
+})
+
+app.get('/operating_comitee', function(req,res){
+  res.render('opcom.ejs')
 })
 
 app.get('/staff', function(req,res){
-  res.render('staff/staff.ejs')
+  res.render('staff.ejs')
 })
-
-app.get('/attendance', function(req,res){
-  res.render('attendance/attendance.ejs')
-})
-
-
-app.get('/', function(req,res){
-  res.render('/main.ejs')
-})
-
-
-
-
-
 
 app.listen(port, ip, function(){
   console.log("Site Is Functional");
