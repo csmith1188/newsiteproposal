4623eadceeaa243a632dd0321f7632f7b1152bf1
Parker Fink <parke2023059@live.ytech.edu>
2023-01-25 11:30:15 -0500

    home page started
    Title and information section added. Buttons started css and link to other pages

--- newsiteproposal/main.js
+++ newsiteproposal/main.js
@@ -2,22 +2,28 @@ const express = require('express')
 
 var app = express()
 const path = require('path')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
 
+
+var port = 1000
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('./static'))
 
 
 
-var port = 1000
-app.get('/', function (req,res) { 
+
+app.get('/', function (req,res) {
        res.render('home.ejs')})
 
-app.listen(port, function () {    
+app.get('/MediaCenter', function(req,res){
+    res.render('MediaCenter/mediaCenter.ejs')
+})
+
+
+
+app.listen(port, function () {
     console.log("Listening on port " + port)
 })
