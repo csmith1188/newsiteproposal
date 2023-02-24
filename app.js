49d116deca7eb92ed332f3d1da59fe0bfef499df
Will Shealer <will23046@live.ytech.edu>
2023-02-24 08:38:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -31,9 +31,6 @@ function convertExcelFileToJsonUsingXlsx() {
         // Convert to json using xlsx
         const tempData = xlsx.utils.sheet_to_json(file.Sheets[sheetNames[i]]);
   
-        // Skip header row which is the colum names
-        tempData.shift();
-  
         // Add the sheet's json to our data array
         parsedData.push(...tempData);
     }
@@ -46,6 +43,7 @@ function convertExcelFileToJsonUsingXlsx() {
   function generateJSONFile(data) {
     try {
     fs.writeFileSync('data.json', JSON.stringify(data))
+    console.log(data.length)
     } catch (err) {
     console.error(err)
     }
