2b56caba2930bc73ca8feeb4dd18e870547dc9fa
Trey <90394867+TFloto@users.noreply.github.com>
2023-03-08 11:28:58 -0500

    3/8 commit
    anything that has changed

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./static'))
+app.use('/fonts', express.static(__dirname + '/fonts'));
 
 
 function convertExcelFileToJsonUsingXlsx() {
@@ -108,7 +109,9 @@ app.get('/athletics', function(req,res){
     res.render('athleticsHome.ejs')
 })
 
-
+app.get('/districts', function(req, res){
+    res.render('districtmap.ejs')
+})
 
 
 
