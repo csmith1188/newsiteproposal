70c8a7a21d815de354c22a6cebfb2c0611d5296a
carme24036 <carme24036@live.ytech.edu>
2023-05-05 14:19:56 -0400

    fix some things

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ app.get('/mediaCenter', function (req,res) {
 
 
 //athletics
-app.get('/athletics', function(req,res){
+app.get('/sports', function(req,res){
     res.render('athleticsHome.ejs')
 })
 
